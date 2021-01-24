16a555cc4318c356dbadb59d732ad2a2d824aa39
koreanstudent <hn12344@naver.com>
2021-01-24 18:13:11 +0900

    vue-router 적용

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -1,6 +1,29 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+// import LoginPage from '@/views/LoginPage.vue';
+// import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
 
-export default new VueRouter();
+export default new VueRouter({
+  mode: 'history', // url에 #이 제거됨
+
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/LoginPage'),
+    },
+    {
+      path: '/signup',
+      component: () => import('@/views/SignupPage'),
+    },
+    {
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
+    },
+  ],
+});
