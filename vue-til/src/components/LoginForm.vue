fa95a9f38cabdea275eba22fde1083a92f8d1e5f
koreanstudent <hn12344@naver.com>
2021-01-25 22:34:55 +0900

    css register

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -1,18 +1,31 @@
 <template>
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="username">id:</label>
-      <input type="text" id="username" v-model="username" />
+  <div class="contents">
+    <div class="form-wrapper form-wrapper-sm">
+      <form @submit.prevent="submitForm" class="form">
+        <div>
+          <label for="username">id:</label>
+          <input id="username" type="text" v-model="username" />
+          <p class="validation-text">
+            <span class="warning" v-if="!isUsernameValid && username">
+              Please enter an email address
+            </span>
+          </p>
+        </div>
+        <div>
+          <label for="password">pw:</label>
+          <input id="password" type="text" v-model="password" />
+        </div>
+        <button
+          :disabled="!isUsernameValid || !password"
+          type="submit"
+          class="btn"
+        >
+          로그인
+        </button>
+      </form>
+      <p class="log">{{ logMessage }}</p>
     </div>
-    <div>
-      <label for="password">pw:</label>
-      <input type="text" id="password" v-model="password" />
-    </div>
-    <button type="submit" :disabled="!isUsernameValid || !password">
-      로그인
-    </button>
-    <p>{{ logMessage }}</p>
-  </form>
+  </div>
 </template>
 
 <script>
@@ -59,4 +72,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.btn {
+  color: white;
+}
+</style>
--- vue-til/src/views/LoginPage.vue
+++ vue-til/src/views/LoginPage.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-    <h1>
-      로그인 페이지
-      <LoginForm></LoginForm>
-    </h1>
+    <h1 class="page-header">로그인 페이지</h1>
+    <LoginForm></LoginForm>
   </div>
 </template>
-
 <script>
 import LoginForm from '@/components/LoginForm.vue';
 
