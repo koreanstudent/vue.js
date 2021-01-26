b5c8b60e98500fbbf32a3122167729eeea97ccc8
koreanstudent <hn12344@naver.com>
2021-01-26 23:52:34 +0900

    axios - interceptors

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,9 +1,21 @@
 import axios from 'axios';
+import { setInterceptors } from './common/interceptors';
 
-// create : 요청을 할때 공통 설정들을 미리 넣을 수 있다.
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-});
+function createInstance() {
+  // create : 요청을 할때 공통 설정들을 미리 넣을 수 있다.
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+    // headers: {
+    //   //헤더에 이미 정의 된 속성 중 하나
+    //   // Authorization: store.state.token,
+    // },
+  });
+  // axios.post('http://localhost:3000)
+  // instance.interceptors.request.use();
+  return setInterceptors(instance);
+}
+
+const instance = createInstance();
 
 function registerUser(userData) {
   //   const url = 'http://localhost:3000/signup';
--- vue-til/src/store/index.js
+++ vue-til/src/store/index.js
@@ -8,6 +8,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     username: '',
+    token: '',
   },
   getters: {
     isLogin(state) {
@@ -20,6 +21,9 @@ export default new Vuex.Store({
     },
     clearUsername(state) {
       state.username = '';
+    },
+    setToken(state, token) {
+      state.token = token;
     },
   },
 });
